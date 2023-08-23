12943493fb942b036c3d45b332eb6b92c4f2ebee
essamcreates <mrm.hussain@hotmail.co.uk>
2023-08-23 16:43:22 +0100

    completed Car module and tests

---
+++ Car.js
@@ -0,0 +1,25 @@
+const Car = function(manufacturer, price, engineType){
+    this.manufacturer = manufacturer;
+    this.price = price;
+    this.engineType = engineType;
+}
+
+Car.prototype.setManufacturer = function(newManufacturer) {
+    this.manufacturer = newManufacturer;
+    }
+    
+    Car.prototype.setPrice = function(newPrice) {
+        this.price = newPrice;
+    }
+
+    Car.prototype.setEngineType = function(engineType) {
+        this.engineType = engineType;
+    }
+        
+    Car.prototype.getManufacturer = function() {return this.manufacturer}
+    Car.prototype.getPrice = function () { return this.price}
+    Car.prototype.getEngineType = function () { return this.engineType}
+
+
+module.exports = Car;  
+
---
+++ Dealership.js
@@ -0,0 +1,6 @@
+const Car = function(dealershipName, capacity, carsInStock){
+    this.dealershipName = dealershipName;
+    this.capacity = capacity;
+    this.carsInStock = carsInStock;
+}
+
